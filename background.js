4df0ffa2f12104b5cd4266a67e261ab1f3db334a
tossj <tossj@users.noreply.github.com>
2018-08-06 15:54:57 -0400

    Add option to redirect reddit.com to old.reddit.com (#2)
    * Added options page for Reddit redirection preference
    
    * Added logic to redirect reddit.com to old.reddit.com
    
    * Added logic to request webRequest and webRequestBlocking permissions
    
    * Added logic to open options on first install
    
    * Fixed options page form not reflecting values in storage
    
    * Fixed webRequest handlers not being registered when starting up browser
    
    * Changed version number label to 1.1

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // set this cookie to make Reddit not show the mobile site on mobile devices
 const noMobileCookie = {
     url: "https://.reddit.com",
@@ -30,9 +32,81 @@ function setCookie() {
     }, onError);
 }
 
+function toggleRedirect(doRedirect) {
+    if (doRedirect) {
+        browser.webRequest.onBeforeRequest.addListener(
+            redirect,
+            {
+                urls: [
+                    'https://www.reddit.com/*',
+                    'https://reddit.com/*',
+                    'http://www.reddit.com/*',
+                    'http://reddit.com/*'
+                ]
+            },
+            [ 'blocking' ]);
+
+        // flush in-memory cache to prevent fetchs from cache w/o redirect
+        browser.webRequest.handlerBehaviorChanged()
+            .then(() => { console.log('In-memory cache flush'); }, onError);
+    } else {
+        const requestedPermissions = {
+            permissions: [ 'webRequest', 'webRequestBlocking' ]
+        };
+        browser.permissions.contains(requestedPermissions)
+            .then((result) => {
+                if (result) {
+                    if (browser.webRequest.onBeforeRequest.hasListener(redirect)) {
+                        browser.webRequest.onBeforeRequest.removeListener(redirect);
+                    }
+                    // revoke webRequest permissions
+                    browser.permissions.remove(requestedPermissions)
+                        .then((result) => {
+                            result
+                                ? console.log('Permissions revoked')
+                                : console.error('Permissions revoke error');
+                        }, onError);
+                }}, onError);
+    }
+}
+
+/**
+ * Listener to redirect www.reddit.com requests to old.reddit.com
+ */
+function redirect(request) {
+    console.log(request.url);
+    const redirectUrl = request.url.replace(
+        /^(?:https|http):\/\/(?:www\.)?reddit\.com/,
+        'https://old.reddit.com');
+    return Promise.resolve({ redirectUrl });
+}
+
 (function () {
     // set the cookie on profile/extension load and for every new tab
     browser.runtime.onInstalled.addListener(setCookie);
     browser.runtime.onStartup.addListener(setCookie);
     browser.tabs.onCreated.addListener(setCookie);
+
+    // check if redirecting is enabled, default to false
+    browser.storage.local.get({ redirect: false })
+        .then((obj) => { toggleRedirect(obj.redirect); }, onError);
+
+    // enable/disable redirect on storage change
+    browser.storage.onChanged.addListener((changes) => {
+        if ('redirect' in changes) {
+            if (changes.redirect.oldValue !== changes.redirect.newValue) {
+                toggleRedirect(changes.redirect.newValue);
+            }
+        }
+    });
+
+    // open preference page onInstall, once
+    browser.runtime.onInstalled.addListener(() => {
+        browser.storage.local.get({ firstInstall: true })
+            .then((obj) => {
+                if (obj.firstInstall) return browser.runtime.openOptionsPage();
+            }).then(() => {
+                return browser.storage.local.set({ firstInstall: false });
+            }).then(() => { return browser.storage.local.get(); })
+            .then((obj) => { console.log(obj); }, onError)});
 })();
---
+++ options/options.js
@@ -0,0 +1,68 @@
+"use strict";
+
+function saveOptions(ev) {
+    const cb = ev.target;
+
+    // disable checkbox before storage
+    toggleForm(true, cb.form);
+
+    // save storage
+    const obj = {};
+    obj[cb.name] = cb.checked;
+
+    // ask for webRequest permissions first, if setting to true
+    const promise = obj[cb.name]
+        ? requestOptionalPermissions()
+        : Promise.resolve();
+    promise.then(() => { return browser.storage.local.set(obj); }, onError)
+        .then(() => { return browser.storage.local.get(); }, onError)
+        .then((obj) => { console.log(obj); }, onError)
+        .finally(() => {
+            // enable checkbox
+            setTimeout(() => { toggleForm(false, cb.form); }, 700);
+        });
+}
+
+/**
+ * Requests the WebExtension permissions listed in the optional_permissions
+ * field of manifest.json.
+ */
+function requestOptionalPermissions() {
+    // webRequest and webRequestBlocking do not prompt the user when requested
+    // seems to also bypass restriction of asking for permissions from embedded
+    // options_ui page set forth in FF v55-61
+    const requestedPermissions = {
+        permissions: [ 'webRequest', 'webRequestBlocking' ]
+    };
+    return browser.permissions.request(requestedPermissions)
+        .then((result) => {
+            result
+                ? console.log('Permissions granted')
+                : console.error('Permissions denied');
+        }, onError);
+}
+
+function onError(error) {
+    console.error(error);
+}
+
+function onSuccess(result) {
+    browser.storage.local.get().then((obj) => { console.log(obj); }, onError);
+}
+
+function toggleForm(disabled, form) {
+    form.className = disabled === true ? 'disabled' : '';
+    for (let i = 0; i < form.elements.length; i++) {
+        form.elements[i].disabled = disabled;
+    }
+}
+
+(function () {
+    // register checkbox event listener
+    const redirectCb = document.getElementById('redirect-reddit');
+    redirectCb.addEventListener('change', saveOptions);
+
+    // fill checkbox with value from storage, default to false
+    browser.storage.local.get({ redirect: false })
+        .then((obj) => { redirectCb.checked = obj.redirect; }, onError);
+}());
